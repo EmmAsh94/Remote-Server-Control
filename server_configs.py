9a28f4d9ec0cd75d0b7fc0b1e2f823554243b8e1
EmmAsh94 <154464430+EmmAsh94@users.noreply.github.com>
2024-01-09 18:12:34 +0100

    Create server_configs.py

---
+++ server_configs.py
@@ -0,0 +1,35 @@
+#################################################################################
+# ************************  server_configaration.py  ************************** #
+#                                                                               #
+#  This is used in Remote_Server_Control_App.py.                                #
+#  Function:                                                                    #
+#    1. configuration details of servers that the Remote Server Control App     #
+#       interacts with.                                                         #
+#    Provided Information (hostname, username and password)                     #
+#                                                                               #
+#                                                                               #
+#    @author Houssem Abdellatif                                                 #
+#################################################################################
+
+SERVER_CONFIGS = [
+        {
+            "hostname": "",
+            "username": "",
+            "password": "",
+
+        },
+        {
+
+            "hostname": "",
+            "username": "",
+            "password": "",
+
+        },
+{
+
+            "hostname": "",
+            "username": "",
+            "password": "",
+
+        }
+    ]
